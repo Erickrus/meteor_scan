8a1e6adb7c59559ffe6cf35cd5430835b3b363b5
Erickrus <hyinghao@hotmail.com>
2022-01-05 11:06:58 +0800

    Add files via upload

---
+++ meteor_scan.py
@@ -0,0 +1,153 @@
+import os
+import glob
+from PIL import Image, ImageDraw
+import math
+import numpy as np
+import sys
+
+from sklearn.cluster import DBSCAN
+from concurrent.futures import ProcessPoolExecutor
+
+
+class MeteorScan:
+    def __init__(self, nProcess):
+        self.nProcess = nProcess
+        self.fps = 8
+        self.gridSize = 32
+        self.blankVideoLen = 2.5
+        self.rectColor = "red"
+
+    def _extract(self, mp4Filename):
+        print("mkdir dump")
+        os.system("mkdir dump")
+        print("mkdir scanned")
+        os.system("mkdir scanned")
+        print("mkdir output")
+        os.system("mkdir output")
+        print("rm -Rf dump/*")
+        os.system("rm -Rf dump/*")
+        mp4R8Filename = mp4Filename.replace(".mp4", ".r%d.mp4" % self.fps)
+        targetFilename = os.path.basename(mp4Filename.replace(".mp4","_%5d.png"))
+        print("ffmpeg -i %s -r %d %s " % (mp4Filename, self.fps, mp4R8Filename))
+        os.system("ffmpeg -i %s -r %d %s " % (mp4Filename, self.fps, mp4R8Filename))
+        print("ffmpeg -i %s dump/%s" %(mp4R8Filename, targetFilename))
+        os.system("ffmpeg -i %s dump/%s" %(mp4R8Filename, targetFilename))
+        print("rm -f %s " % mp4R8Filename)
+        os.system("rm -f %s " % mp4R8Filename)
+
+
+        targetFilenamePattern = mp4Filename.replace(".mp4","_*.png")
+        imFilenames = sorted(list(glob.glob("dump/%s" % targetFilenamePattern)))
+        
+        return imFilenames
+
+    def _mp_scan(self, srcImFilenames, mp4Filename, start, nSize):
+        size = None
+        scanned = []
+        print("processing: "+mp4Filename)
+        imFilenames = srcImFilenames[start:start+nSize]
+        nImages = len(imFilenames)
+        # build backgrounds
+        if size == None:
+            size = Image.open(imFilenames[0]).size
+        grids = {}
+        for i in range(3*int(self.fps)):
+            imFilename = imFilenames[i]
+            im = Image.open(imFilename).convert("L")
+            im = np.array(im)
+            for x in range(size[0]//self.gridSize):
+                for y in range(size[1] //self.gridSize):
+                    gridId = "%d_%d" % (x, y)
+                    piece = im[y*self.gridSize:(y+1)*self.gridSize,x*self.gridSize:(x+1)*self.gridSize]
+                    if gridId in grids:
+                        grids[gridId].append(piece.tolist())
+                    else:
+                        grids[gridId] = [piece.tolist()]
+
+        
+        for i in range(nImages):
+            imFilename = imFilenames[i]
+            print(imFilename)
+            found = False
+            imColor = Image.open(imFilename)
+            draw = ImageDraw.Draw(imColor)
+            im = Image.open(imFilename).convert("L")
+            im = np.array(im)
+            for x in range(size[0]//self.gridSize):
+                for y in range(size[1] //self.gridSize):
+                    gridId = "%d_%d" % (x, y)
+                    piece = im[y*self.gridSize:(y+1)*self.gridSize,x*self.gridSize:(x+1)*self.gridSize]
+                    v = np.array(grids[gridId])
+                    v = np.mean(v, axis=0)
+                    diff = (piece - v).max()
+                    totDiff = np.abs(piece - v)
+                    totDiff = totDiff[totDiff > diff - 20].size
+
+                    if diff > 52 and totDiff > 6:
+                        scanned.append(imFilename)
+                        print("%s,%d,%d" % (imFilename,x*self.gridSize,y*self.gridSize))
+                        draw.rectangle((x*self.gridSize,y*self.gridSize,(x+1)*self.gridSize,(y+1)*self.gridSize), outline =self.rectColor)
+                        found = True
+                    del grids[gridId][0]
+                    grids[gridId].append(piece.tolist())
+            if found:
+                imColor.save(imFilename.replace("dump", "scanned"))
+
+        return scanned
+
+    def _scan(self, mp4Filename):
+        srcImFilenames = self._extract(mp4Filename)
+        nImages = len(srcImFilenames)
+        batchSize = int(math.ceil(float(nImages) / float(self.nProcess)))
+        futures = []
+        with ProcessPoolExecutor(max_workers=self.nProcess) as executor:
+            for i in range(self.nProcess):
+                startPos = i * batchSize
+                endPos = min((i+1) * batchSize, nImages)
+                nSize = endPos - startPos
+                print(nImages, i, startPos, nSize)
+                future = executor.submit(self._mp_scan, srcImFilenames, mp4Filename, startPos, nSize)
+                futures.append(future)
+
+            for i in range(self.nProcess):
+                futures[i].result()
+
+    def _cut(self, mp4Filename):
+        targetFilenamePattern = mp4Filename.replace(".mp4","_*.png")
+        imFilenames = sorted(list(glob.glob("scanned/%s" % targetFilenamePattern)))
+
+        rawPositions = []
+        for i in range(len(imFilenames)):
+            imFilename = imFilenames[i]
+            rawPositions.append([1., float(os.path.basename(imFilename)[:-4].split("_")[-1])])
+
+        dbscan = DBSCAN(eps=self.blankVideoLen*float(self.fps), min_samples=1)
+        clusterLabels = dbscan.fit_predict(rawPositions)
+        cuts = {}
+        for i in clusterLabels:
+            if str(clusterLabels[i]) in cuts:
+                cuts[str(clusterLabels[i])].append(rawPositions[i][1] / float(self.fps))
+            else:
+                cuts[str(clusterLabels[i])] = [rawPositions[i][1] / float(self.fps)]
+
+        for cutPositions in cuts:
+            startPos = int(np.array(cuts[cutPositions]).min()-self.blankVideoLen)
+            endPos   = int(np.array(cuts[cutPositions]).max()+self.blankVideoLen)
+
+            outputVideoFilename = "output/"+mp4Filename.replace(".mp4", "_"+str(cutPositions)+".mp4")
+            print("ffmpeg -y -i %s -ss %d -to %d -c copy %s" %(mp4Filename, startPos, endPos, outputVideoFilename))
+            os.system("ffmpeg -y -i %s -ss %d -to %d -c copy %s" %(mp4Filename, startPos, endPos, outputVideoFilename))
+
+    def scanAndCut(self, mp4Filename):
+        self._scan(mp4Filename)
+        self._cut(mp4Filename)
+
+if __name__ == "__main__":
+    mp4Filename = sys.argv[1]
+    nProcess = 1
+    if len(sys.argv)>2:
+        nProcess = int(sys.argv[2])
+    MeteorScan(nProcess = nProcess).scanAndCut(mp4Filename)
+    
+
+
